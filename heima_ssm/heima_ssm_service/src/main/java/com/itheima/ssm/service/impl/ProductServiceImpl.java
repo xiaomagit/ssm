87256f89b35ebe3834ecdeaff9340c975f268dbe
xiaoma <xiaoma@0620.com>
2019-06-19 20:40:19 +0800

    环境搭建

---
+++ heima_ssm/heima_ssm_service/src/main/java/com/itheima/ssm/service/impl/ProductServiceImpl.java
@@ -0,0 +1,13 @@
+package com.itheima.ssm.service.impl;
+
+import com.itheima.ssm.domain.Product;
+import com.itheima.ssm.service.ProductService;
+
+import java.util.List;
+
+public class ProductServiceImpl implements ProductService {
+    @Override
+    public List<Product> findAll() throws Exception {
+        return null;
+    }
+}
